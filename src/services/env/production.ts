386f4d27cf21a1f256bcae33d06b1c901ee1d6c9
Le Gia Uy <leuy091104@gmail.com>
2026-01-10 17:17:05 +0700

    fix: thay secret_key

--- src/services/env/production.ts
+++ src/services/env/production.ts
@@ -2,12 +2,13 @@
  * .env.dev
  */
 
-import type { Env } from '@/interface/env'
+import type { Env } from "@/interface/env";
 
 const ENV: Env = {
-  secret_key: '0cf5516973a145929ff36d3303183e5f',
-  merchant: 'https://api.merchant.vn/v1',
-  contact: 'https://api-contact.merchant.vn'
-}
+  // secret_key: '0cf5516973a145929ff36d3303183e5f',
+  secret_key: "31c2d699735340f78bcd97cb0abe1d26",
+  merchant: "https://api.merchant.vn/v1",
+  contact: "https://api-contact.merchant.vn",
+};
 
-export default ENV
+export default ENV;
